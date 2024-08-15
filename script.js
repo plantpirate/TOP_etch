fd91a975bb6ccfd102dee13cca73f070df3b6c72
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-15 18:15:09 -0500

    Comment JS div creation out to practice normal creation with flex

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
 const divContainer = document.querySelector(".container");
 // const divsAmount = prompt("What size grid do you want?");
-const initGame = function () {
-  for (let i = 0; i < 4; i++) {
-    const divRow = document.createElement("div");
-    newDiv.classList.add("row");
-    newDiv.style.border = "1px solid black";
-    divContainer.appendChild(newDiv);
-  }
-  //   for (let i = 1; i <= divsAmount; i++) {
-  //     if (divsAmount <= 100) {
-  //       const newDiv = document.createElement("div");
-  //       newDiv.classList.add("div");
-  //       newDiv.style.border = "1px solid black";
-  //       newDiv.style.minWidth = "10%";
-  //       newDiv.style.flex = "1 1 auto";
-  //       divContainer.appendChild(newDiv);
-  //     }
-  //   }
-};
+// const initGame = function () {
+//   for (let i = 0; i < 4; i++) {
+//     const divRow = document.createElement("div");
+//     newDiv.classList.add("row");
+//     newDiv.style.border = "1px solid black";
+//     divContainer.appendChild(newDiv);
+//   }
+//   //   for (let i = 1; i <= divsAmount; i++) {
+//   //     if (divsAmount <= 100) {
+//   //       const newDiv = document.createElement("div");
+//   //       newDiv.classList.add("div");
+//   //       newDiv.style.border = "1px solid black";
+//   //       newDiv.style.minWidth = "10%";
+//   //       newDiv.style.flex = "1 1 auto";
+//   //       divContainer.appendChild(newDiv);
+//   //     }
+//   //   }
+// };
 
-initGame();
+// initGame();
