03545c03b15a85672234cd2d41909e2013a24919
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-15 18:14:25 -0500

    Implement start of JS div creation

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
 const divContainer = document.querySelector(".container");
 // const divsAmount = prompt("What size grid do you want?");
 const initGame = function () {
+  for (let i = 0; i < 4; i++) {
+    const divRow = document.createElement("div");
+    newDiv.classList.add("row");
+    newDiv.style.border = "1px solid black";
+    divContainer.appendChild(newDiv);
+  }
   //   for (let i = 1; i <= divsAmount; i++) {
   //     if (divsAmount <= 100) {
   //       const newDiv = document.createElement("div");
