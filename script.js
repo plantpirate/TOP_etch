14117bf9fdbb9147063055e62a8cfcb1f15c298d
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-14 20:18:27 -0500

    Change for loop to only create set amount for further easy testing

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const divContainer = document.querySelector(".container");
-const divsAmount = prompt("What size grid do you want?");
-for (let i = 1; i <= divsAmount; i++) {
+// const divsAmount = prompt("What size grid do you want?");
+for (let i = 1; i <= 16; i++) {
   const newDiv = document.createElement("div");
   newDiv.style.border = "1px solid black";
   newDiv.style.width = "2rem";
