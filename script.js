84c2b356eaf608cda56941f7faca18b9d67452ac
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-14 21:23:54 -0500

    Place div creating code block in if statement to ensure only 100 possible

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
 const divContainer = document.querySelector(".container");
 const divsAmount = prompt("What size grid do you want?");
 for (let i = 1; i <= divsAmount; i++) {
-  const newDiv = document.createElement("div");
-  newDiv.classList.add("div");
-  newDiv.style.border = "1px solid black";
-  newDiv.style.minWidth = "10%";
-  newDiv.style.flex = "1 1 auto";
-  divContainer.appendChild(newDiv);
+  if (divsAmount <= 100) {
+    const newDiv = document.createElement("div");
+    newDiv.classList.add("div");
+    newDiv.style.border = "1px solid black";
+    newDiv.style.minWidth = "10%";
+    newDiv.style.flex = "1 1 auto";
+    divContainer.appendChild(newDiv);
+  }
 }
