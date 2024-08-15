53e877a7146b57f293e5d8006091940f3340543e
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-14 20:13:43 -0500

    Soft test for loop to create new divs with size and coloring

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 const divContainer = document.querySelector(".container");
+
+for (let i = 0; i < 6; i++) {
+  const newDiv = document.createElement("div");
+  newDiv.style.backgroundColor = "yellow";
+  newDiv.style.width = "2rem";
+  newDiv.style.height = "2rem";
+  divContainer.appendChild(newDiv);
+}
