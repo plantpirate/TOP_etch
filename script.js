01c006ad42121d816e3b41abb8db953f3395b39d
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-14 20:17:13 -0500

    Change for loop initial start

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 const divContainer = document.querySelector(".container");
 const divsAmount = prompt("What size grid do you want?");
-for (let i = 0; i <= divsAmount; i++) {
+for (let i = 1; i <= divsAmount; i++) {
   const newDiv = document.createElement("div");
-  newDiv.style.backgroundColor = "yellow";
+  newDiv.style.border = "1px solid black";
   newDiv.style.width = "2rem";
   newDiv.style.height = "2rem";
   divContainer.appendChild(newDiv);
