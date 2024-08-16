7f5f91bb8f430d9f0edb9b2dd9369b0baa46144b
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-15 21:48:10 -0500

    Replace JS divs styles with CSS styling

--- script.js
+++ script.js
@@ -4,17 +4,10 @@ const createInitGrid = function () {
   for (let i = 0; i < 16; i++) {
     const divRow = document.createElement("div");
     divRow.classList.add("row");
-    divRow.style.display = "flex";
-    divRow.style.flexGrow = "1";
-    divRow.style.alignItems = "stretch";
-    divRow.style.border = "1px solid black";
     divContainer.appendChild(divRow);
     for (j = 0; j < 16; j++) {
       const newSquare = document.createElement("div");
       newSquare.classList.add("square");
-      newSquare.style.flexGrow = "1";
-      newSquare.style.alignContent = "stretch";
-      newSquare.style.border = "1px solid black";
       divRow.appendChild(newSquare);
     }
   }
