9960aabcdd9fe3367ee66be10c551108e9e4282c
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-15 22:08:26 -0500

    Attach event listener to button that runs createUserGrid function

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
 const divContainer = document.querySelector(".container");
+const userGridBtn = document.querySelector(".user");
+
+const createUserGrid = function () {
+  divContainer.innerHTML = "";
+  const gridHeight = prompt("How tall do you want your grid to be?");
+  const gridWidth = prompt("How wide do you want your grid to be?");
+
+  for (let i = 0; i < gridHeight; i++) {
+    const divRow = document.createElement("div");
+    divRow.classList.add("row");
+    divContainer.appendChild(divRow);
+    for (j = 0; j < gridWidth; j++) {
+      const newSquare = document.createElement("div");
+      newSquare.classList.add("square");
+      divRow.appendChild(newSquare);
+    }
+  }
+};
 
 const createInitGrid = function () {
   for (let i = 0; i < 16; i++) {
@@ -17,21 +35,6 @@ const createInitGrid = function () {
       sq.style.backgroundColor = "yellow";
     });
   });
+  userGridBtn.addEventListener("click", createUserGrid);
 };
 createInitGrid();
-
-const createUserGrid = function () {
-  const gridHeight = prompt("How tall do you want your grid to be?");
-  const gridWidth = prompt("How wide do you want your grid to be?");
-
-  for (let i = 0; i < gridHeight; i++) {
-    const divRow = document.createElement("div");
-    divRow.classList.add("row");
-    divContainer.appendChild(divRow);
-    for (j = 0; j < gridWidth; j++) {
-      const newSquare = document.createElement("div");
-      newSquare.classList.add("square");
-      divRow.appendChild(newSquare);
-    }
-  }
-};
