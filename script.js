a9a91f35059ee0ed6266206fab2e56580afafb6f
plantpirate <152737849+plantpirate@users.noreply.github.com>
2024-08-15 21:49:52 -0500

    Remove styling lines from createUserGrid function

--- script.js
+++ script.js
@@ -27,17 +27,10 @@ const createUserGrid = function () {
   for (let i = 0; i < gridHeight; i++) {
     const divRow = document.createElement("div");
     divRow.classList.add("row");
-    divRow.style.display = "flex";
-    divRow.style.flexGrow = "1";
-    divRow.style.alignItems = "stretch";
-    divRow.style.border = "1px solid black";
     divContainer.appendChild(divRow);
     for (j = 0; j < gridWidth; j++) {
       const newSquare = document.createElement("div");
       newSquare.classList.add("square");
-      newSquare.style.flexGrow = "1";
-      newSquare.style.alignContent = "stretch";
-      newSquare.style.border = "1px solid yellow";
       divRow.appendChild(newSquare);
     }
   }
